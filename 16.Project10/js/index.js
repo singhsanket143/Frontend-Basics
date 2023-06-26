5f770526725b52366299af5bad57e30df7780ea2
Sanket Singh <singhsanket143@gmail.com>
2023-06-26 21:33:30 +0530

    added codes

---
+++ 16.Project10/js/index.js
@@ -0,0 +1,24 @@
+console.log("Index js loaded");
+
+async function fetchCategories() {
+    // this function is marked async so this will also return a promise
+    const response = await fetch("https://fakestoreapi.com/products/categories");
+    const data = await response.json();
+    return data;
+}
+
+async function populateCtaegories() {
+    const categories = await fetchCategories();
+    const categoryList = document.getElementById("categoryList");
+    categories.forEach(category => {
+        const categoryHolder = document.createElement("div");
+        const categoryLink = document.createElement("a");
+        categoryLink.href = "#";
+        categoryLink.textContent = category; // setting the category name as the text of the anchor tag
+        categoryHolder.classList.add("category-item", "d-flex", "align-items-center", "justify-content-center");
+        categoryHolder.appendChild(categoryLink);
+        categoryList.appendChild(categoryHolder);
+    });
+}
+
+populateCtaegories();
